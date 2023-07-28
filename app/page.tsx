aa3d74aaaeb1f02d868a21ca75837c029bdc50bb
Binod Budhathoki <100508632+Binod69@users.noreply.github.com>
2023-07-28 10:07:09 +0545

    fixed an issue

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
 import Banner from './Components/Banner/Banner';
 import Business from './Components/Business/Business';
 import Info from './Components/Info/Info';
-// import svg from '../public/img/rocket-svgrepo-com.svg'
+import Unlimited from './Components/Unlimited/Unlimited';
 export default function Home() {
   return (
     <>
       <Banner />
       <Info />
       <Business />
+      <Unlimited />
     </>
   );
 }
