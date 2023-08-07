b668cbe9b4503ae59bcbd78d1644a5e6f93c8253
Binod Budhathoki <100508632+Binod69@users.noreply.github.com>
2023-08-07 10:47:57 +0545

    added new components in service page

--- app/screen/ServiceScreen.tsx
+++ app/screen/ServiceScreen.tsx
@@ -1,6 +1,7 @@
 import {
   CounterService,
   Outstanding,
+  Pricing,
   ServiceBanner,
   Unlimited,
 } from '../Components';
@@ -14,6 +15,15 @@ const Service = () => {
       <Unlimited
         title={'Focus on cutting-edge technologies and cost-quality efficiency'}
         description={'HELPING ENTREPRENEURS MAKE MORE MONEY'}
+      />
+      <Pricing
+        title={'Choose the right plan for you'}
+        para={
+          'Scale your software operations through a custom engineering team. Meet the demand of your company’s'
+        }
+        priceTitle1={'Personal'}
+        priceTitle2={'StartUp'}
+        priceTitle3={'Business'}
       />
     </>
   );
