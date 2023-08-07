5bc9975eb3f638bf238efbe5cc06e7b9e2db1783
Binod Budhathoki <100508632+Binod69@users.noreply.github.com>
2023-08-07 17:39:34 +0545

    fixed some errors

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,5 +1,10 @@
+import { Metadata } from 'next';
 import Service from '../screen/ServiceScreen';
 
+export const metadata: Metadata = {
+  title: 'Services-Digi Dolphins',
+  description: 'About digi dolphins',
+};
 const page = () => {
   return (
     <>
