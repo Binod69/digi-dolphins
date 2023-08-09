59cd84e7ac2395c42b69bec2a96d4bd243bbb793
Binod Budhathoki <100508632+Binod69@users.noreply.github.com>
2023-08-09 20:25:47 +0545

    added new components

--- app/Components/AboutComponents/NewsLetter/NewsLetter.tsx
+++ app/Components/AboutComponents/NewsLetter/NewsLetter.tsx
@@ -1,18 +1,43 @@
 'use client';
+import React, { FormEvent } from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import newsimage from '../../../../public/img/footer-bg-004.jpeg';
+import Form from 'react-bootstrap/Form';
 import styles from './newsletter.module.css';
-const NewsLetter = () => {
+
+const NewsLetter: React.FC = () => {
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+  };
+
   return (
-    <>
-      <Container className={styles.con}>
-        <Row>
-          <Col></Col>
-        </Row>
-      </Container>
-    </>
+    <Container className={styles.con}>
+      <Row>
+        <Col>
+          <article className={styles.articleCon}>
+            <h3 className={styles.title}>
+              Receive the latest technology & business news in your inbox
+            </h3>
+          </article>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Form className={styles.form}>
+            <Form.Control
+              className={styles.input}
+              type="text"
+              placeholder="Your email address"
+            />
+            <Form.Control type="button" />
+            {/* <button className={styles.btn} onClick={handleSubmit}>
+              Sign up
+            </button> */}
+          </Form>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
