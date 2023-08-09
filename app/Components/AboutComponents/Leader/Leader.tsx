e2ca540db59a88d6914bd23c28109915703f3bb4
Binod Budhathoki <100508632+Binod69@users.noreply.github.com>
2023-08-09 13:50:21 +0545

    optimized images

--- app/Components/AboutComponents/Leader/Leader.tsx
+++ app/Components/AboutComponents/Leader/Leader.tsx
@@ -1,24 +1,51 @@
 'use client';
+import Image from 'next/image';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import ux from '../../../../public/img/image4.png';
+import leaderData from './leaderData';
 import styles from './leader.module.css';
 
 const Leader = () => {
   return (
     <>
-      <Container>
-        <Row>
-          <Col>
-            <article>
-              <h2>
-                As leaders in developing sustainable solutions, the future of
-                blockchain is in our history
-              </h2>
-            </article>
-          </Col>
-        </Row>
-      </Container>
+      <div className={styles.con}>
+        <Container>
+          <Row>
+            <Col lg={12}>
+              <article className={styles.articleCon}>
+                <h2 className={styles.title}>
+                  As leaders in developing sustainable solutions, the future of
+                  software development is in our history
+                </h2>
+              </article>
+            </Col>
+          </Row>
+          <Row className={styles.imgCon}>
+            {leaderData &&
+              leaderData.map((item) => (
+                <Col key={item.id} lg={6}>
+                  <figure>
+                    <Image
+                      src={item.image}
+                      alt="ui.webp"
+                      quality={100}
+                      loading="lazy"
+                      width={500}
+                      height={500}
+                      style={{ objectFit: 'cover' }}
+                      className={styles.img}
+                    />
+                    <figcaption className={styles.imgPara}>
+                      {item.title}
+                    </figcaption>
+                  </figure>
+                </Col>
+              ))}
+          </Row>
+        </Container>
+      </div>
     </>
   );
 };
---
+++ app/Components/AboutComponents/Leader/leaderData.ts
@@ -0,0 +1,28 @@
+const mockData: Array<{
+  id: number;
+  title: string;
+  image: string;
+}> = [
+  {
+    id: 1,
+    title: 'ui/ux design',
+    image: '/img/image4.webp',
+  },
+  {
+    id: 2,
+    title: 'web apps',
+    image: '/img/image1.webp',
+  },
+  {
+    id: 3,
+    title: 'mobile app',
+    image: '/img/image2.webp',
+  },
+  {
+    id: 4,
+    title: 'digital marketing',
+    image: '/img/image3.webp',
+  },
+];
+
+export default mockData;
