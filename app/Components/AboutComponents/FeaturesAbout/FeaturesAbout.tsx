f8cbb2df7d12136437e541e8595c40a5551d905e
Binod Budhathoki <100508632+Binod69@users.noreply.github.com>
2023-08-09 17:02:32 +0545

    added new components

--- app/Components/AboutComponents/FeaturesAbout/FeaturesAbout.tsx
+++ app/Components/AboutComponents/FeaturesAbout/FeaturesAbout.tsx
@@ -2,15 +2,66 @@
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import styles from './growthabout.module.css';
+import { MdOutlineDone } from 'react-icons/md';
+import styles from './featuresabout.module.css';
 const FeaturesAbout = () => {
   return (
     <>
-      <Container>
-        <Row>
-          <Col></Col>
-        </Row>
-      </Container>
+      <div className={styles.con}>
+        <Container>
+          <Row>
+            <Col lg={6}>
+              <article className={styles.articleCon}>
+                <h4 className={styles.title}>
+                  Growth strategies to be effective & competitive
+                </h4>
+                <p className={styles.para}>
+                  Money should never sit still. Achieve capital efficiency with
+                  our apps: as a leading DeFi yield aggregator, we offer
+                  first-rate yield optimization and risk teaching strategies.
+                  Get the best yield by using, integrating or building on top of
+                  Idle’s products.
+                </p>
+                <div className={styles.listCon}>
+                  <div>
+                    <ul className={styles.ul}>
+                      <li className={styles.li}>
+                        <MdOutlineDone size={20} className={styles.icon} />
+                        Boost Seo
+                      </li>
+                      <li className={styles.li}>
+                        <MdOutlineDone size={20} className={styles.icon} />
+                        Visual Reviews
+                      </li>
+                      <li className={styles.li}>
+                        <MdOutlineDone size={20} className={styles.icon} />
+                        Social Sharing
+                      </li>
+                    </ul>
+                  </div>
+                  <div>
+                    <ul className={styles.ul}>
+                      <li className={styles.li}>
+                        <MdOutlineDone size={20} className={styles.icon} />{' '}
+                        Manage Buying
+                      </li>
+                      <li className={styles.li}>
+                        <MdOutlineDone size={20} className={styles.icon} />{' '}
+                        Buying Protection
+                      </li>
+                      <li className={styles.li}>
+                        <MdOutlineDone size={20} className={styles.icon} />{' '}
+                        Virtual Card
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </article>
+            </Col>
+            <Col lg={6}></Col>
+          </Row>
+        </Container>
+      </div>
     </>
   );
 };
